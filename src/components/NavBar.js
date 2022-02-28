4a2e09f2ce0684d9f9010683e958c97352e03e49
MaraVega <maraluzvega@gmail.com>
2022-02-27 21:34:27 -0300

    navBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+function NavBar(){
+    return (
+        <>
+        <h1>LIBRERIA EDEN</h1>
+        <li><a href="#">home</a></li>
+        <li><a href="#">nosotros</a></li>
+        <li><a href="#">productos</a></li>
+        <li><a href="#">contacto</a></li>
+        </>
+    )
+}
+
+export default NavBar
